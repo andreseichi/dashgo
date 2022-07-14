565a993b680723d7e49b2e7592f5460584b0992e
andreseichi <andre_seichi@hotmail.com>
2022-07-14 18:17:35 -0300

    utilizando ssr para lista de users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
   Tr,
   useBreakpointValue,
 } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
 import NextLink from 'next/link';
 import { useState } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
@@ -25,12 +26,14 @@ import Pagination from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import { api } from '../../services/api';
 
-import { useUsers } from '../../services/hooks/useUsers';
+import { getUsers, useUsers } from '../../services/hooks/useUsers';
 import { queryClient } from '../../services/queryClient';
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [currentPage, setCurrentPage] = useState(1);
-  const { data, isLoading, isFetching, error } = useUsers(currentPage);
+  const { data, isLoading, isFetching, error } = useUsers(currentPage, {
+    initialData: users,
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -151,3 +154,13 @@ export default function UserList() {
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+
+  return {
+    props: {
+      users,
+    },
+  };
+};

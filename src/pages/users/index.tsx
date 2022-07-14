65b4a4f6bbf0d7634569dc0b951ea72201ca4bd4
andreseichi <andre_seichi@hotmail.com>
2022-07-14 12:02:53 -0300

    hook useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,36 +18,15 @@ import {
 import Link from 'next/link';
 import { useEffect } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
-import { useQuery } from 'react-query';
 
 import { Header } from '../../components/Header';
 import Pagination from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
-import { api } from '../../services/api';
+
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery(
-    'users',
-    async () => {
-      const { data } = await api.get('users');
-
-      const users = data.users.map((user) => {
-        return {
-          ...user,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 10, // 10 seconds
-    }
-  );
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
